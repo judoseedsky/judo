a3f0421eaa4b6e93fc67ac2bf4ab5b7823cedc64
Judo Sky <judoseedsky@gmail.com>
2022-05-29 23:26:04 -0400

    dump

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='page'>
+        <h1>Judo بذرة Sky</h1>
+      </div>
+
+      <div className='details'>
+        <h2>Software Dev</h2>
+        <br/>
+        <br/>
+        <h2>♪♬Producer♪♬</h2>
+        <br/>
+        <br/>
+        <h2>νομοθέτης</h2>
+        <br/>
+        <br/>
+      </div>
+      {/* <img id='buddha' src='https://thumbs.gfycat.com/ImpassionedTameBullfrog-max-1mb.gif' alt='buddha'></img> */}
+    </div>
+  );
+}
+
+export default App;
