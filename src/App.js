c73980125875324491b7820e714284e3ea740f3a
Judo Sky <judoseedsky@gmail.com>
2022-06-05 17:33:39 -0400

    many edits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+/* eslint-disable no-dupe-keys */
 import './App.css';
+// import brahma from "./img/brahma.png";
+import React from "react";
 
 function App() {
+
+  // const style = {
+  //   textDecoration: 'none',
+  //   ":hover": {
+  //     textDecoration: 'line-through'
+  //   }
+  // }
+
+  // const devStyles = {
+  //   textDecoration: 'line-through',
+  //   backgroundImage: `url(${brahma})`,
+  //   backgroundSize: "contain",
+  //   backgroundRepeat: 'no-repeat',
+  //   backgroundPosition: 'center',
+  //   backgroundPosition: 'absolute',
+  //   height: '100vh',
+  //   width: '50vw'
+  // }
+
+  // const [devStyle, setDevStyle] = React.useState(style);
+
+  // const handleDevClick = () => {
+  //   devStyle.textDecoration === style.textDecoration ? setDevStyle(devStyles) : setDevStyle(style)
+
+  // }
+
+
   return (
     <div className="App">
       <div className='page'>
@@ -8,13 +38,13 @@ function App() {
       </div>
 
       <div className='details'>
-        <h2>Software Dev</h2>
+        <h2 id='dev'>Software Dev</h2>
         <br/>
         <br/>
-        <h2>♪♬Producer♪♬</h2>
+        <h2 id='prod'>♪♬Producer♪♬</h2>
         <br/>
         <br/>
-        <h2>νομοθέτης</h2>
+        <h2 id='greek'>νομοθέτης</h2>
         <br/>
         <br/>
       </div>
